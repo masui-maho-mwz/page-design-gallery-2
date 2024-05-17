9f4bcfe0f1e4f5589d29b5c1af5870e00e12297a
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-17 10:06:05 +0900

    fix: スライダーに画像をセット

--- src/app/mega-menu-header/slider/index.tsx
+++ src/app/mega-menu-header/slider/index.tsx
@@ -1,13 +1,17 @@
 'use client';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
+import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-
 import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import styles from './styles.module.css';
+
+import cat from '../../../../public/cat.jpg';
+import dog from '../../../../public/dog.jpg';
+import sea from '../../../../public/sea.jpg';
+import shadow from '../../../../public/shadow.jpg';
+import skyEarth from '../../../../public/sky-earth.jpg';
 
 export const Slider = () => {
   return (
@@ -17,18 +21,24 @@ export const Slider = () => {
         slidesPerView={'auto'}
         loop={true}
         speed={2000}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        autoplay={{ delay: 2500, disableOnInteraction: false }}
         modules={[Autoplay, Pagination, Navigation]}
       >
-        <SwiperSlide className={styles.slide1}>Slide 1</SwiperSlide>
-        <SwiperSlide className={styles.slide2}>Slide 2</SwiperSlide>
-        <SwiperSlide className={styles.slide3}>Slide 3</SwiperSlide>
-        <SwiperSlide className={styles.slide4}>Slide 4</SwiperSlide>
-        <SwiperSlide className={styles.slide5}>Slide 5</SwiperSlide>
-        <SwiperSlide className={styles.slide6}>Slide 6</SwiperSlide>
+        <SwiperSlide>
+          <Image src={dog} alt="dog" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={sea} alt="sea" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={cat} alt="cat" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={shadow} alt="shadow" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src={skyEarth} alt="sky-earth" />
+        </SwiperSlide>
       </Swiper>
     </div>
   );
