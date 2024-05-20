6ece7fc255bb5e90764b192713e6e1c4a2ea709b
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-20 14:18:31 +0900

    fix: headerの大枠を作成

--- src/app/dashboard-menu/header/index.tsx
+++ src/app/dashboard-menu/header/index.tsx
@@ -3,7 +3,14 @@ import styles from './styles.module.css';
 export const Header = () => {
   return (
     <header className={styles.root}>
-      <div>header</div>
+      <div className={styles.wrap}>
+        <button className={styles.search}>虫眼鏡あいこん</button>
+        <div className={styles.buttons}>
+          <button className={styles.button}>○</button>
+          <button className={styles.button}>△</button>
+          <button className={styles.button}>◻︎</button>
+        </div>
+      </div>
     </header>
   );
 };
