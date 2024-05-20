5097a1196ad50459f27ce95d66ecbc0dd038d31b
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-20 18:41:26 +0900

    fix: HeaderにSearchBarを追加

--- src/app/dashboard-menu/header/index.tsx
+++ src/app/dashboard-menu/header/index.tsx
@@ -1,10 +1,46 @@
+'use client';
+
+import { useEffect, useRef, useState } from 'react';
 import styles from './styles.module.css';
 
 export const Header = () => {
+  const [isSearch, setIsSearch] = useState(false);
+  const ref = useRef<HTMLDivElement | null>(null);
+
+  const toggleSearch = () => {
+    setIsSearch((prev) => !prev);
+  };
+
+  const handleClickOutside = (event: MouseEvent) => {
+    if (ref.current && event.target instanceof Node && !ref.current.contains(event.target)) {
+      setIsSearch(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSearch) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isSearch]);
+
   return (
     <header className={styles.root}>
       <div className={styles.wrap}>
-        <button className={styles.search}>虫眼鏡あいこん</button>
+        <button className={styles.searchIcon} onClick={toggleSearch}>
+          虫眼鏡あいこん
+        </button>
+        {isSearch && (
+          <div className={`${styles.searchBar} ${isSearch ? styles.rootVisible : ''}`} ref={ref}>
+            <input type="text" placeholder="Search…" className={styles.input} />
+            <button className={styles.searchButton}>Search</button>
+          </div>
+        )}
         <div className={styles.buttons}>
           <button className={styles.button}>○</button>
           <button className={styles.button}>△</button>
