14de050d7e1d431eccf3ea5c14e67c273613000f
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-22 12:02:51 +0900

    delete: 不要なスタイル、タグを削除

--- src/app/normal-header/components/main/right-side-panel/index.tsx
+++ src/app/normal-header/components/main/right-side-panel/index.tsx
@@ -5,64 +5,62 @@ import styles from './styles.module.css';
 export const RightSidePanel = () => {
   return (
     <div className={styles.root}>
-      <div>
-        <ul>
-          <li className={styles.item}>
-            <a href="" className={styles.link}>
-              セクション１
-            </a>
-          </li>
-          <li className={styles.item}>
-            <a href="" className={styles.link}>
-              セクション２
-            </a>
-            <ul className={styles.index}>
-              <li className={styles.item}>
-                <a href="" className={styles.link}>
-                  サブセクション2-1
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className={styles.item}>
-            <a href="" className={styles.link}>
-              セクション３
-            </a>
-          </li>
-          <li className={styles.item}>
-            <a href="" className={styles.link}>
-              セクション４
-            </a>
-            <ul className={styles.index}>
-              <li className={styles.item}>
-                <a href="" className={styles.link}>
-                  サブセクション4-1
-                </a>
-              </li>
-              <li className={styles.item}>
-                <a href="" className={styles.link}>
-                  サブセクション4-2
-                </a>
-              </li>
-              <li className={styles.item}>
-                <a href="" className={styles.link}>
-                  サブセクション4-3
-                </a>
-              </li>
-              <li className={styles.item}>
-                <a href="" className={styles.link}>
-                  サブセクション4-4
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li className={styles.item}>
-            <a href="" className={styles.link}>
-              セクション５
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul>
+        <li className={styles.item}>
+          <a href="" className={styles.link}>
+            セクション１
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href="" className={styles.link}>
+            セクション２
+          </a>
+          <ul className={styles.index}>
+            <li className={styles.item}>
+              <a href="" className={styles.link}>
+                サブセクション2-1
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.item}>
+          <a href="" className={styles.link}>
+            セクション３
+          </a>
+        </li>
+        <li className={styles.item}>
+          <a href="" className={styles.link}>
+            セクション４
+          </a>
+          <ul className={styles.index}>
+            <li className={styles.item}>
+              <a href="" className={styles.link}>
+                サブセクション4-1
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a href="" className={styles.link}>
+                サブセクション4-2
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a href="" className={styles.link}>
+                サブセクション4-3
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a href="" className={styles.link}>
+                サブセクション4-4
+              </a>
+            </li>
+          </ul>
+        </li>
+        <li className={styles.item}>
+          <a href="" className={styles.link}>
+            セクション５
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
