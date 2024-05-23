2eae8e5d24487189af4a0353c52874e6e6b3f600
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-23 12:37:52 +0900

    fix: データの記載をUIコンポーネントファイルからdata.ts に変更

--- src/app/normal-header/components/header/navigation/index.tsx
+++ src/app/normal-header/components/header/navigation/index.tsx
@@ -1,15 +1,7 @@
+import { navItems } from '@/app/normal-header/components/header/navigation/nav-items-data';
 import styles from './styles.module.css';
 
 export const Navigation = () => {
-  const navItems = [
-    { id: 1, name: 'ナビ１' },
-    { id: 2, name: 'ナビ２' },
-    { id: 3, name: 'ナビ３' },
-    { id: 4, name: 'ナビ４' },
-    { id: 5, name: 'ナビ５', isNew: true },
-    { id: 6, name: 'ナビ６' },
-  ];
-
   return (
     <nav className={styles.root}>
       {navItems.map((item) => (
---
+++ src/app/normal-header/components/header/navigation/nav-items-data.ts
@@ -0,0 +1,8 @@
+export const navItems = [
+  { id: 1, name: 'ナビ１' },
+  { id: 2, name: 'ナビ２' },
+  { id: 3, name: 'ナビ３' },
+  { id: 4, name: 'ナビ４' },
+  { id: 5, name: 'ナビ５', isNew: true },
+  { id: 6, name: 'ナビ６' },
+];
