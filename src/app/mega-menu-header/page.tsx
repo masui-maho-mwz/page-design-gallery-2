e2f65666802ec3856dfb1aa9a4f4feec2a0ce94e
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-23 14:27:58 +0900

    fix: layout.tsxを使うように修正（mega-menu-header）

--- src/app/mega-menu-header/page.tsx
+++ src/app/mega-menu-header/page.tsx
@@ -1,18 +1,12 @@
 import { Slider } from '@/app/mega-menu-header/components/slider';
 
-import { Footer } from '@/app/mega-menu-header/components/footer';
-import { Header } from '@/app/mega-menu-header/components/header';
 import styles from './styles.module.css';
 
 export default function MegaMenuHeaderHome() {
   return (
     <div>
-      <Header />
-      <div>
-        <Slider />
-        <div className={styles.main}>main</div>
-      </div>
-      <Footer />
+      <Slider />
+      <div className={styles.main}>main</div>
     </div>
   );
 }
