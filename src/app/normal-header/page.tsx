f7a6d460a30d92657da5b792a0f64025e6fa5f52
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-24 17:54:50 +0900

    fix: normal-headerの右のセクションをコンポーネント化して、データは呼び出し元でインポートしてPropsとして渡すように修正

--- src/app/normal-header/page.tsx
+++ src/app/normal-header/page.tsx
@@ -1,12 +1,13 @@
 import { MainContents } from '@/app/normal-header/components/main/main-contents';
 import { RightSidePanel } from '@/app/normal-header/components/main/right-side-panel';
+import { sections } from '@/app/normal-header/sections-data';
 import styles from './styles.module.css';
 
 export default function NormalHeaderHome() {
   return (
     <main className={styles.root}>
       <MainContents />
-      <RightSidePanel />
+      <RightSidePanel sections={sections} />
     </main>
   );
 }
