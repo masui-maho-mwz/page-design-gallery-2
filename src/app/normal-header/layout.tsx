9cd6b0f308522e53ee03a5ad121b2b65209f7cd7
masui-maho-mwz <m.masui@miwaza.co.jp>
2024-05-24 16:39:27 +0900

    fix: normal-headerにだけ作成していたスタイルをプロジェクト共通スタイルにするためglobals.cssにまとめた

--- src/app/normal-header/layout.tsx
+++ src/app/normal-header/layout.tsx
@@ -1,7 +1,6 @@
 import { Footer } from '@/app/normal-header/components/footer';
 import { Header } from '@/app/normal-header/components/header';
 import type { Metadata } from 'next';
-import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Page Design Gallery',
